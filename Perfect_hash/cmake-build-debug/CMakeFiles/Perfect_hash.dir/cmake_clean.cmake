1ac0a9d1e79f830d0f066a17f0d4e1af6f25fb22
Sht0rmy <70601237+Sht0rmy@users.noreply.github.com>
2022-06-26 23:15:24 +0300

    Add files via upload

---
+++ Perfect_hash/cmake-build-debug/CMakeFiles/Perfect_hash.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Perfect_hash.dir/main.cpp.obj"
+  "Perfect_hash.exe"
+  "Perfect_hash.exe.manifest"
+  "Perfect_hash.pdb"
+  "libPerfect_hash.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Perfect_hash.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
